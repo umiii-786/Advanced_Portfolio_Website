4c6b24ad196c031d62d2cf23bc5137f5ec209974
umair jatt <umiiideveloper@gmail.com>
2025-03-08 01:57:21 +0500

    set the layout maintained the folder  structured of updated portfolio

--- Navbar.js
+++ Navbar.js
@@ -10,3 +10,25 @@ document.querySelector('.hamburgur').addEventListener('click',()=>{
          links.style.top="-200px"
     }
 })
+
+
+function Readmore(btn){
+    let element=document.querySelector('.AboutList_option')
+    let heading=document.getElementById('aboutHeading')
+    let screenSize=window.screen.width
+    
+    let display=window.getComputedStyle(element, null).display
+    if(display=="none"){
+        element.style.display="block"
+        heading.style.position="absolute"
+        heading.style.transform="translate(-50%, -50%)";
+        btn.innerText="Read Less....!"
+    }
+    else{
+        element.style.display="none"
+        heading.style.position="static"
+        heading.style.transform="unset";
+        btn.innerText="Read More....!"
+    }
+
+}
